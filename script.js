542e06ffb329bb66687abe3dfc12196dcab876b5
hiya dosi <hiya.23bce11720@vitbhopal.ac.in>
2025-07-14 12:37:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function toggleForm() {
   const popup = document.getElementById("formPopup");
   popup.style.display = popup.style.display === "flex" ? "none" : "flex";
+  if (popup.style.display === "flex") {
+    popup.style.justifyContent = "center";
+    popup.style.alignItems = "center";
+  }
 }
